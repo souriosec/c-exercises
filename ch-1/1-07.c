8f77c0680a5c3cdd50f85bb769fb7f75058cd4a4
souriosec <gbsribeiro97@gmail.com>
2025-02-24 03:53:08 +0900

    Exercise 1-7

---
+++ ch-1/1-07.c
@@ -0,0 +1,13 @@
+/*
+ * K&R The C Programming Language
+ *
+ * Exercise 1-7. Write a program to print the value of EOF.
+ *
+ * By souriosec
+ */
+
+#include <stdio.h>
+int main() {
+  printf("%d\n", EOF);
+  return 0;
+}
