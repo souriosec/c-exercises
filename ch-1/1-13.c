125c856c513ada576aa78d25b24f7a33c4c70c7c
souriosec <gbsribeiro97@gmail.com>
2025-02-27 00:18:04 +0900

    Working on 1-13

---
+++ ch-1/1-13.c
@@ -0,0 +1,20 @@
+/*
+ * K&R The C Programming Language
+ *
+ * Exercise 1-13. Write a program to print a histogram of the lengths of words
+ * in its input.
+ *
+ * By souriosec
+ */
+
+#include <stdio.h>
+
+int main() {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    putchar(c);
+  }
+
+  return 0;
+}
